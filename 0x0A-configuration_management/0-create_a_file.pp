dbecd5b171429f958677da404a5968e1349a9b0a
Daniel <komolafedanielolaoluwa@gmail.com>
2024-03-29 08:53:21 -0700

    .

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Create file in /tmp
+# Creating a file in /tmp
 
-file { '/tmp/school' :
-    ensure  =>  'file'
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-    content => 'I love Puppet',
-    }
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love puppet',
+}
